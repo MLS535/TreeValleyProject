1c4e7db346a502b0295163e206b27d0ef917e40b
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2022-02-22 19:43:51 +0100

    Página shop

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,267 +1,69 @@
 <template>
   <!-- ACCORDEON-->
-<div class="blog fontbody text-black-50">
-  <div class="container d-flex  ">
+  <div class="blog fontbody text-black-50">
 
-    <div class="container mb-5 ">
-      <div class="row cards">
-        <!--justify-content-center -->
-        <!-- MUY IMPORTANTE NO TOCAR ORDER-1!!!!!!!!!!-->
-
-        <div class="col-lg-2 order-1 mt-3  blogaccordeon">
-
-          <h4 class="archiveh accordion-header text-center rounded-top p-2">Archive</h4>
-          <div class="accordion">
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button bg-white" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
-                        aria-controls="panelsStayOpen-collapseOne">
-                  2023
-                </button>
-              </h2>
-              <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
-                   aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                  <ul class="list-unstyled link-secondary mx-4">
-                    <li><a href="#">December</a></li>
-                    <li><a href="#">November</a></li>
-                    <li><a
-                        href="#">October</a></li>
-                    <li><a href="#">Septempter</a></li>
-                    <li><a href="#">August</a></li>
-                    <li><a href="#">July</a></li>
-                    <li><a href="#">June</a></li>
-                    <li><a href="#">May</a></li>
-                    <li><a href="#">April</a></li>
-                    <li><a href="#">March</a></li>
-                    <li><a href="#">February</a></li>
-                    <li><a href="#">January</a></li>
-
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button bg-white collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                  2023
-                </button>
-              </h2>
-              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                   data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <ul class="list-unstyled text-decoration-none">
-                    <li><a href="#">December</a></li>
-                    <li><a href="#">November</a></li>
-                    <li><a
-                        href="#">October</a></li>
-                    <li><a href="#">Septempter</a></li>
-                    <li><a href="#">August</a></li>
-                    <li><a href="#">July</a></li>
-                    <li><a href="#">June</a></li>
-                    <li><a href="#">May</a></li>
-                    <li><a href="#">April</a></li>
-                    <li><a href="#">March</a></li>
-                    <li><a href="#">February</a></li>
-                    <li><a href="#">January</a></li>
-
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingthree">
-                <button class="accordion-button  bg-white collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                  2023
-                </button>
-              </h2>
-              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                   data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <ul class="list-unstyled text-decoration-none ">
-                    <li><a href="#">December</a></li>
-                    <li><a href="#">November</a></li>
-                    <li><a
-                        href="#">October</a></li>
-                    <li><a href="#">Septempter</a></li>
-                    <li><a href="#">August</a></li>
-                    <li><a href="#">July</a></li>
-                    <li><a href="#">June</a></li>
-                    <li><a href="#">May</a></li>
-                    <li><a href="#">April</a></li>
-                    <li><a href="#">March</a></li>
-                    <li><a href="#">February</a></li>
-                    <li><a href="#">January</a></li>
-
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingfour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
-                  2023
-                </button>
-              </h2>
-              <div id="collapsefour" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                   data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                  <ul class="list-unstyled text-decoration-none">
-                    <li><a href="#">December</a></li>
-                    <li><a href="#">November</a></li>
-                    <li><a href="#">October</a></li>
-                    <li><a href="#">Septempter</a></li>
-                    <li><a href="#">August</a></li>
-                    <li><a href="#">July</a></li>
-                    <li><a href="#">June</a></li>
-                    <li><a href="#">May</a></li>
-                    <li><a href="#">April</a></li>
-                    <li><a href="#">March</a></li>
-                    <li><a href="#">February</a></li>
-                    <li><a href="#">January</a></li>
-
-                  </ul>
-                </div>
+    <div class="container my-5">
+      
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <img src="../assets/images/blog-thumb1.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Producto 1</h5>
+              <p class="card-text">Descripción del producto 1.</p>
+              <div class="d-flex flex-row justify-content-between">
+                <h3 class="text-muted text-start">25 €</h3>
+                <div class="btn btn-primary">Al carrito</div>
               </div>
             </div>
           </div>
         </div>
-
-
-        <!--CARDS -->
-        <div class="col-7 m-1">
-          <div class="container p-2">
-
-            <div class="card">
-              <div class="row gx-0 mx-0">
-                <div class="col-xl-3">
-                  <a href="#"><img class="m-3 rounded mx-auto d-block" src="../assets/images/blog-thumb1.png"
-                                   alt=""></a>
-                </div>
-                <div class="col-xl-8">
-                  <div class="card-body">
-                    <h3 class="card-title">This is just a place holder</h3>
-                    <p class="card-text">
-                      You can remove any link to our website from this website template, you're free
-                      to
-                      use
-                      this
-                      website template without linking back to us.
-                    </p>
-                    <hr>
-                    <p class="card-text d-flex justify-content-between"></p>
-                      <small class="text-muted text-start ">Aug 28 by Kurimao</small>
-
-                      <span class="badge bg-warning text-center"><small>000 views</small></span>
-                  </div>
-                </div>
+        <div class="col">
+          <div class="card">
+            <img src="../assets/images/blog-thumb2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Producto 2</h5>
+              <p class="card-text">Descripción del producto 2.</p>
+              <div class="d-flex flex-row justify-content-between">
+                <h3 class="text-muted text-start">25 €</h3>
+                <div class="btn btn-primary">Al carrito</div>
               </div>
             </div>
           </div>
-
-          <div class="container p-2">
-            <div class="card">
-              <div class="row gx-0 mx-0 ">
-                <div class="col-xl-3">
-                  <a href="#"><img class="m-3 rounded mx-auto d-block" src="../assets/images/blog-thumb2.png"
-                                   alt=""></a>
-                </div>
-                <div class="col-xl-8">
-                  <div class="card-body">
-                    <h3 class="card-title">This is just a place holder</h3>
-                    <p class="card-text">
-                      You can remove any link to our website from this website template, you're free
-                      to
-                      use
-                      this
-                      website template without linking back to us.
-                    </p>
-                    <hr>
-                    <p class="card-text d-flex justify-content-between"></p>
-                      <small class="text-muted text-start">Aug 28 by Kurimao</small>
-
-                      <span class="badge bg-warning text-center"><small>000 views</small></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-          <div class="container p-2">
-            <div class="card">
-              <div class="row row gx-0 mx-0 ">
-                <div class="col-xl-3">
-                  <a href="#"><img class="m-3 rounded mx-auto d-block" src="../assets/images/blog-thumb3.png"
-                                   alt=""></a>
-                </div>
-                <div class="col-xl-8">
-                  <div class="card-body">
-                    <h3 class="card-title">This is just a place holder</h3>
-                    <p class="card-text">
-                      You can remove any link to our website from this website template, you're free
-                      to
-                      use
-                      this
-                      website template without linking back to us.
-                    </p>
-                    <hr>
-                    <p class="card-text d-flex justify-content-between"></p>
-                      <small class="text-muted text-start">Aug 28 by Kurimao</small>
-
-                      <span class="badge bg-warning text-center"><small>000 views</small></span>
-
-
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="container p-2">
-
-            <div class="card">
-              <div class="row row gx-0 mx-0 ">
-                <div class="col-xl-3">
-                  <a href="#"><img class="m-3 rounded mx-auto d-block" src="../assets/images/blog-thumb4.png"
-                                   alt=""></a>
-                </div>
-                <div class="col-xl-8">
-                  <div class="card-body">
-                    <h3 class="card-title">This is just a place holder</h3>
-                    <p class="card-text">
-                      You can remove any link to our website from this website template, you're free
-                      to
-                      use
-                      this
-                      website template without linking back to us.
-                    </p>
-                    <hr>
-                    <p class="card-text d-flex justify-content-between"></p>
-                      <small class="text-muted text-start">Aug 28 by Kurimao</small>
-
-                      <span class="badge bg-warning text-center"><small>000 views</small></span>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-          </div>
-
-
         </div>
-
-        <div class="vr removethisvr p-0 ml-2"></div>
+        <div class="col">
+          <div class="card">
+            <img src="../assets/images/blog-thumb3.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Producto 3</h5>
+              <p class="card-text">Descripción del producto 3.</p>
+              <div class="d-flex flex-row justify-content-between">
+                <h3 class="text-muted text-start">25 €</h3>
+                <div class="btn btn-primary">Al carrito</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-2">
+          <div class="card">
+            <img src="../assets/images/blog-thumb4.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Producto 4</h5>
+              <p class="card-text">Descripción del producto 4.</p>
+              <div class="d-flex flex-row justify-content-between">
+                <h3 class="text-muted text-start">25 €</h3>
+                <div class="btn btn-primary">Al carrito</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        
       </div>
+
     </div>
-  </div>
+
   <div class="container p-1 mb-5">
     <div class="row">
       <div class="col">
